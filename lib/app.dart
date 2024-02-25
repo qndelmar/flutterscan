3503039e385fc7f0dfa374e4795794a9a528f6a0
reincarndeity <mihoyodaiteei@gmail.com>
2024-02-25 15:42:50 +0300

    temp remove analyze

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:barcode_reader/router/router.dart';
 import 'package:barcode_reader/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class BarcodeReaderApp extends StatelessWidget {
   const BarcodeReaderApp({super.key});
